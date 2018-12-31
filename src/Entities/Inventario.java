c2f0d2868df3ccad4b2d60199b6fc6a5bdc27a1e
L Angel Albeño <sanyi0@hotmail.es>
2018-12-31 08:16:41 -0600

    Pre Inventario con fecha

--- src/Entities/Inventario.java
+++ src/Entities/Inventario.java
@@ -6,8 +6,6 @@
 package Entities;
 
 import java.io.Serializable;
-import java.math.BigInteger;
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
